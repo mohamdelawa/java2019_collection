10d95e78d5d7adac7ccfbd79278b5525a968a474
mohamdelawa <46392133+mohamdelawa@users.noreply.github.com>
2019-04-29 23:33:50 +0300

    collection

--- src/Ass3/Main.java
+++ src/Ass3/Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Ass3;
 
 import java.util.Comparator;
@@ -10,10 +6,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-/**
- *
- * @author Abdullah Shublaq
- */
 public class Main {
 
     public static void main(String[] args) {
